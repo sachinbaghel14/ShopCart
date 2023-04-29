44b42a5138a7d6b20b68726b4ca37300c652c83e
sachin <sb.officalsachin@gmail.com>
2023-04-29 19:18:20 +0530

    complete header UI

---
+++ src/pages/Coponents/ProductCard.jsx
@@ -0,0 +1,10 @@
+import "./ProductCard.css"
+
+
+export function ProductCard(){
+    return(
+        <div>
+            {/* <img src={} className="card-img-top" alt="..."></img> */}
+        </div>
+    )
+}
